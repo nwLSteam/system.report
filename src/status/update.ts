d0b1ed22052e41468da7aa3b5d3ed8bfde997aa6
nwLSteam <25863799+nwLSteam@users.noreply.github.com>
2024-04-26 13:51:28 +0200

    Add stub status update function

---
+++ src/status/update.ts
@@ -0,0 +1,45 @@
+"use server";
+
+import API from "@data/api";
+import { Core } from "bungie-api-ts";
+import { HistoricalStatusPoint, SystemStatus } from "@contracts/HistoricalStatus";
+
+export const getStatusReport = async (): Promise<HistoricalStatusPoint> => {
+	const StatusError = ( error: string ): HistoricalStatusPoint => {
+		return {
+			timestampISO: ( new Date() ).toISOString(),
+			error,
+			systems: undefined,
+		};
+	};
+
+	let response;
+	try {
+		response = await Core.getCommonSettings( API.$http );
+	} catch ( e ) {
+		console.error( e );
+		return StatusError( "Status system unreachable." );
+	}
+
+	if ( !( [ 0, 1 ].includes( response.ErrorCode ) ) ) {
+		return StatusError( `Status system errored: ${response.ErrorStatus}` );
+	}
+
+	const systems: SystemStatus[] =
+		      Object.entries( response.Response.systems )
+		            .map( ( [ system, status ] ) => (
+			            {
+				            system,
+				            status: {
+					            enabled: status.enabled,
+					            reachable: undefined,
+				            },
+			            } satisfies SystemStatus
+		            ) );
+
+	return {
+		timestampISO: ( new Date() ).toISOString(),
+		error: undefined,
+		systems,
+	};
+};
