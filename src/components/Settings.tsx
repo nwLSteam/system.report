2a21638e1e227237ddd26038edf6ae1bf07053ea
nwLSteam <25863799+nwLSteam@users.noreply.github.com>
2026-03-11 15:01:16 +0100

    Modernize React rendering

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,120 +1,134 @@
-import { CoreSettingsConfiguration } from "bungie-api-ts/core";
-import React, { ReactElement } from "react";
+import { CoreSettingsConfiguration, type CoreSystem } from "bungie-api-ts/core";
+import { useMemo } from "react";
 import "./Settings.scss";
 import SystemListElement, { SystemListElementType } from "./SystemListElement";
 
-const MAJOR_SYSTEMS: string[] = [
-	"Destiny2",
-	"D2Vendors",
-	"D2PublicMilestones",
-	"D2MilestoneContent",
-	"D2Milestones",
-	"D2Characters",
-	"D2Profiles",
-	"D2Items",
-	// "D2ReturnAvailablePlugs",
-	"D2ReadActions",
-	"D2SubmitReport",
-	"D2EquipItem",
-	"D2TransferItem",
-	"D2PullFromPostmaster",
-	"D2SetItemLockState",
-	"D2InsertPlugsFree",
-	"DestinyClanSearch",
-	"DestinyLinkedProfiles",
-	"ClaimSeasonPassReward",
-	"D2Rewards",
-	"DestinyClans",
-	"Clans",
-	"AccountCreation",
-	"SteamIdAuth",
-	"EpicIdAuth",
-	"PSNAuth",
-	"XuidAuth",
-	"Profiles",
-	"Careers",
-	"Content",
-	"Activities",
-	"Messages",
-];
+const MAJOR_SYSTEMS = new Set<string>( [
+    "Destiny2",
+    "D2Vendors",
+    "D2PublicMilestones",
+    "D2MilestoneContent",
+    "D2Milestones",
+    "D2Characters",
+    "D2Profiles",
+    "D2Items",
+    // "D2ReturnAvailablePlugs",
+    "D2ReadActions",
+    "D2SubmitReport",
+    "D2EquipItem",
+    "D2TransferItem",
+    "D2PullFromPostmaster",
+    "D2SetItemLockState",
+    "D2InsertPlugsFree",
+    "DestinyClanSearch",
+    "DestinyLinkedProfiles",
+    "ClaimSeasonPassReward",
+    "D2Rewards",
+    "DestinyClans",
+    "Clans",
+    "AccountCreation",
+    "SteamIdAuth",
+    "EpicIdAuth",
+    "PSNAuth",
+    "XuidAuth",
+    "Profiles",
+    "Careers",
+    "Content",
+    "Activities",
+    "Messages",
+] as const );
 
-function is_major( name: string ) {
-	return MAJOR_SYSTEMS.includes( name );
+interface CoreSystemMapping {
+    key: string,
+    system: CoreSystem
 }
 
 function Settings( props: { data: CoreSettingsConfiguration } ) {
-	let enabled: ReactElement[] = [];
-	let disabled: ReactElement[] = [];
-	let disabled_major: ReactElement[] = [];
+    const { enabled, disabled, disabledMajor } = useMemo(
+        () => Object
+            .entries( props.data.systems )
+            .map( ( [key, system] ) => ( { key, system } ) )
+            .sort( ( a, b ) => a.key.localeCompare( b.key ) )
+            .reduce(
+                ( acc, item ) => {
+                    if ( item.system.enabled ) {
+                        acc.enabled.push( item );
+                    } else if ( MAJOR_SYSTEMS.has( item.key ) ) {
+                        acc.disabledMajor.push( item );
+                    } else {
+                        acc.disabled.push( item );
+                    }
+                    return acc;
+                },
+                {
+                    enabled: [] as CoreSystemMapping[],
+                    disabled: [] as CoreSystemMapping[],
+                    disabledMajor: [] as CoreSystemMapping[],
+                },
+            ),
+        [] );
 
-	const system_keys = Object.keys( props.data.systems ).sort( ( a, b ) => {
-		return a.localeCompare( b );
-	} );
+    return (
+        <div className={"settings wrapper"}>
+            {( () => {
+                if ( disabledMajor.length > 0 ) {
+                    return <>
+                        <h2 className={"settings error"}>Major systems disabled:</h2>
+                        <ul className={"settings list"}>{
+                            disabledMajor.map( e => (
+                                <SystemListElement
+                                    key={e.key}
+                                    name={e.key}
+                                    enabled={SystemListElementType.DISABLED_MAJOR}
+                                />
+                            ) )
+                        }</ul>
+                    </>;
+                }
 
-	console.log( system_keys );
-
-	for ( const system of system_keys ) {
-		let current = props.data.systems[system];
-
-		if ( current.enabled ) {
-			console.log( `${system} is up` );
-
-			enabled.push( <SystemListElement key={system}
-			                                 enabled={SystemListElementType.ENABLED}
-			                                 name={system} /> );
-		} else {
-			console.log( `${system} is down` );
-			if ( is_major( system ) ) {
-				disabled_major.push( <SystemListElement key={system}
-				                                        enabled={SystemListElementType.DISABLED_MAJOR}
-				                                        name={system} /> );
-			} else {
-				disabled.push( <SystemListElement key={system}
-				                                  enabled={SystemListElementType.DISABLED}
-				                                  name={system} /> );
-			}
-
-		}
-	}
-
-	return <>
-		<div className={"settings wrapper"}>
-
-			{( () => {
-				if ( disabled_major.length > 0 ) {
-					return <>
-						<h2 className={"settings error"}>Major systems disabled:</h2>
-						<ul className={"settings list"}>{disabled_major}</ul>
-					</>;
-				}
-
-				return <>
-					<h2 className={"settings allgood neon"}>All major systems online!</h2>
-					<span className={"settings help"}>
-						Experiencing issues? Check <a href={"https://twitter.com/BungieHelp"}
-						                              target={"_blank"} rel={"noreferrer"}>@BungieHelp</a>, alternatively pray to your preferred deity.
+                return <>
+                    <h2 className={"settings allgood neon"}>All major systems online!</h2>
+                    <span className={"settings help"}>
+						Experiencing issues? Check <a href={"https://twitter.com/BNGServerStatus"}
+                                                      target={"_blank"} rel={"noreferrer"}>@BNGServerStatus</a>,
+                        alternatively pray to your preferred deity.
 					</span>
-				</>;
-			} )()}
+                </>;
+            } )()}
 
-			<hr className={"divider"} />
+            <hr className={"divider"} />
 
-			<div className={"settings other"}>
-				<h2>Disabled systems:</h2>
-				<details>
-					<summary>Click to expand</summary>
-					<ul className={"settings list"}>{disabled}</ul>
-				</details>
+            <div className={"settings other"}>
+                <h2>Disabled systems:</h2>
+                <details>
+                    <summary>Click to expand</summary>
+                    <ul className={"settings list"}>{
+                        disabled.map( e => (
+                            <SystemListElement
+                                key={e.key}
+                                name={e.key}
+                                enabled={SystemListElementType.DISABLED}
+                            />
+                        ) )
+                    }</ul>
+                </details>
 
-
-				<h2>Enabled systems:</h2>
-				<details>
-					<summary>Click to expand</summary>
-					<ul className={"settings list"}>{enabled}</ul>
-				</details>
-			</div>
-		</div>
-	</>;
+                <h2>Enabled systems:</h2>
+                <details>
+                    <summary>Click to expand</summary>
+                    <ul className={"settings list"}>{
+                        enabled.map( e => (
+                            <SystemListElement
+                                key={e.key}
+                                name={e.key}
+                                enabled={SystemListElementType.ENABLED}
+                            />
+                        ) )
+                    }</ul>
+                </details>
+            </div>
+        </div>
+    );
 }
 
 export default Settings;
